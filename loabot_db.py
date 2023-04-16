38456c85a90c909e7a588a44b742b21147be47c8
Felix Kiefer <felikiefer@web.de>
2023-04-16 11:13:36 +0200

    modified sql commands

--- loabot_db.py
+++ loabot_db.py
@@ -30,6 +30,11 @@ class LBDB:
         self.cur.execute(f'INSERT INTO user(name) VALUES ("{user}")')
         self.con.commit()
     
+    def add_raids(self, name, modes, member, rtype):
+        # modes  must be in format '{"modes":["Normal Mode, 1370","...", ...]}'
+        self.cur.execute(f'INSERT INTO raids(name, modes, member, type) VALUES ("{name}","{modes}","{member}","{rtype}")')
+        self.con.commit()
+    
     def add_chars(self, chars, cl, user):
         self.cur.execute(f'INSERT INTO chars(user_id, char_name, class) VALUES((SELECT id FROM user WHERE name="{user}"), "{chars}", "{cl}")')
         self.con.commit()
